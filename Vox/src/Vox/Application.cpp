8b86e9426831f908ff8df6946d1775846950dd49
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-04-24 12:40:58 +0700

    Add GLFW Events and Dispatching

--- Vox/src/Vox/Application.cpp
+++ Vox/src/Vox/Application.cpp
@@ -1,21 +1,31 @@
 #include "voxpch.h"
 #include "Application.h"
 
-#include "Vox/Events/ApplicationEvent.h"
 #include "Vox/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Vox {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
     Application::Application()
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
     }
     
     Application::~Application()
     {
     }
+
+	void Application::OnEvent(Event& e)
+ 	{
+ 		EventDispatcher dispatcher(e);
+ 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+ 
+ 		VOX_CORE_TRACE("{0}", e.ToString());
+ 	}
 
     void Application::Run()
     {
@@ -27,4 +37,9 @@ namespace Vox {
         }
     }
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+ 	{
+ 		m_Running = false;
+ 		return true;
+ 	}
 }
--- Vox/src/Vox/Application.h
+++ Vox/src/Vox/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Vox/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Vox {
@@ -13,7 +15,11 @@ namespace Vox {
         virtual ~Application();
 
         void Run();
+
+        void OnEvent(Event& e);
     private:
+        bool OnWindowClose(WindowCloseEvent& e);
+
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
     };
