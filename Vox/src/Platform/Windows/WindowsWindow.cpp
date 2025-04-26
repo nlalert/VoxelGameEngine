1afe8a59b571d8fdc4421110deba973366ffb236
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-04-26 11:22:28 +0700

    added glad

--- Vox/src/Platform/Windows/WindowsWindow.cpp
+++ Vox/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "Vox/Events/MouseEvent.h"
 #include "Vox/Events/KeyEvent.h"
 
+#include <glad/glad.h>
+
 namespace Vox {
 
     static bool s_GLFWInitialized = false;
@@ -48,6 +50,8 @@ namespace Vox {
 
         m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
         glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		VOX_CORE_ASSERT(status, "Failed to initialize Glad!");
         glfwSetWindowUserPointer(m_Window, &m_Data);
         SetVSync(true);
 
